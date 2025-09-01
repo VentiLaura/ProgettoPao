ae3708dee123358a90674212f2aba6711ce109ce
VentiLaura <lauraventurini04@gmail.com>
2025-09-01 11:11:02 +0200

    tolto file .avif

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
 #include "View/mainWindow.cpp"
 
-int main(){
-    
+int main(int argc, char *argv[]){
+    QApplication videogameLibrary(argc, argv);
+/*    mainWindow window;
+    window.show(); */
+    return videogameLibrary.exec();
 }
