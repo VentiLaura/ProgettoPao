ccbc17dd42c5b5d89d34a241842be5f1c299b603
Kibbino <dabik.cure@gmail.com>
2025-09-02 15:31:52 +0200

    right widget, pagecontainer, page widget creati

---
+++ View/MainRightWidget.h
@@ -0,0 +1,11 @@
+#ifndef MAINRIGHTWIDGET_H
+#define MAINRIGHTWIDGET_H
+#include <QMainWindow>
+#include <QPushButton>
+class MainRightWidget: public QWidget {
+    Q_OBJECT
+    public:
+        MainRightWidget(QWidget* = nullptr);
+    private:
+};
+#endif
---
+++ View/PageWidget.h
@@ -0,0 +1,12 @@
+#ifndef PAGEWIDGET_H
+#define PAGEWIDGET_H
+#include <QMainWindow>
+#include <QPushButton>
+#include "../LogicModel/Products/Product.h"
+class PageWidget: public QWidget {
+    Q_OBJECT
+    public:
+        PageWidget(QWidget* = nullptr, std::vector<product::Product*>);
+    private:
+};
+#endif
