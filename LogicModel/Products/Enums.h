0d72f7bea97a5d71bbf3323fe32eb875e16367a4
VentiLaura <lauraventurini04@gmail.com>
2025-09-06 15:10:15 +0200

    Aggiunti prodotti json

--- LogicModel/Products/Enums.h
+++ LogicModel/Products/Enums.h
@@ -5,6 +5,7 @@ namespace product {
 
 enum class Genre {
     Action,
+    Adventure,
     RPG,
     Platform,
     Puzzle,
@@ -18,7 +19,8 @@ enum class Genre {
     Horror,
     Gacha,
     Simulator,
-    Battle_royale
+    Battle_royale, 
+    Coperativo
 };
 enum class Console_type {
     Xbox,
